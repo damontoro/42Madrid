532291c240b5d41e4a3e4522eae78ae19dfeac02
David Montoro couso <dmontoro@c1r11s1.42madrid.com>
2022-08-21 21:05:02 +0200

    Añadidos los headers

--- ft_printf/parse_hexa.c
+++ ft_printf/parse_hexa.c
@@ -1,13 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_hexa.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmontoro <dmontoro@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/21 20:58:37 by dmontoro          #+#    #+#             */
+/*   Updated: 2022/08/21 21:04:40 by dmontoro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include"printf.h"
+
+char	*ft_itoh(int num)
+{
+	char	*buffer;
+	char	*ret;
+	
+	buffer = "123456789ABCDEFGH";
+	
+}
 
 int	parse_hexa(const char c, va_list list)
 {
-	char	*arg;
+	int	arg;
+	char *write;
 
 	if(c != 'X' && c != 'x')
 		return -1;
-	
-	arg = va_arg(list, char *);
+	arg = va_arg(list, int);
+	write = ft_itoh(arg);
 	ft_putstr_fd(arg, 1);
 	return (1);
 }
