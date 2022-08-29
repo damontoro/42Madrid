888a5b2b26f5153575c049543ca6cc35d73601e3
David Montoro couso <dmontoro@c1r13s3.42madrid.com>
2022-08-29 08:32:47 +0200

    Quitado funciones extras de utils

--- ft_printf/utils.c
+++ ft_printf/utils.c
@@ -6,16 +6,11 @@
 /*   By: dmontoro <dmontoro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 07:53:11 by dmontoro          #+#    #+#             */
-/*   Updated: 2022/08/29 08:14:46 by dmontoro         ###   ########.fr       */
+/*   Updated: 2022/08/29 08:31:37 by dmontoro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
-
-void	ft_putchar_fd(char c, int fd)
-{
-	write(fd, &c, sizeof(char));
-}
 
 static void	recursive_magic(int n, int fd)
 {
@@ -28,45 +23,7 @@ static void	recursive_magic(int n, int fd)
 	}
 }
 
-void	ft_putnbr_fd(int n, int fd)
-{
-	if (n == -2147483648)
-		ft_putstr_fd("-2147483648", fd);
-	else
-	{
-		if (n < 0)
-		{
-			ft_putchar_fd('-', fd);
-			n = -n;
-		}
-		recursive_magic(n, fd);
-	}
-}
 void	ft_putunbr_fd(unsigned int n, int fd)
 {
-	if (n < 10)
-		ft_putchar_fd(n + '0', fd);
-	else
-	{
-		recursive_magic(n / 10, fd);
-		ft_putchar_fd(n % 10 + '0', fd);
-	}
-}
-
-
-void	ft_putstr_fd(char *s, int fd)
-{
-	if (!s)
-		return ;
-	write(fd, s, sizeof(char) * ft_strlen(s));
-}
-
-size_t	ft_strlen(const char *s)
-{
-	int	i;
-
-	i = 0;
-	while (s[i])
-		++i;
-	return (i);
+	recursive_magic(n, fd);
 }
