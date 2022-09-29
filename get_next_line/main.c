7768564009577b54c526de12f64ce7177b308908
Souls <dmonto01@ucm.es>
2022-09-29 11:07:13 +0200

    Un pequeño main para hacer pruebas y un archivo

--- get_next_line/main.c
+++ get_next_line/main.c
@@ -1,13 +1,18 @@
-#include"get_next_line.h"
+#include"get_next_line_bonus.h"
 #include<stdio.h>
 #include<unistd.h>
 #include <fcntl.h> 
 #include <string.h>
+#include <sys/time.h>
 
 int main(){
-	int fd = open("/home/souls/Proyectos42/42Madrid/get_next_line/pruebas.txt", O_RDONLY);
-	
-	while ( printf("%s", get_next_line(fd)) > 0){}
+	int fd1= open("/home/souls/Proyectos42/42Madrid/get_next_line/pruebas.txt", O_RDONLY);
+	int fd2= open("/home/souls/Proyectos42/42Madrid/get_next_line/pruebas2.txt", O_RDONLY);
+
+	//printf("FD_SETSIZE=%d", FOPEN_MAX);
+	while(printf("%s", get_next_line(fd1)) > 0){
+		printf("%s", get_next_line(fd2));
+	}
 	
 	return 0;
 }
