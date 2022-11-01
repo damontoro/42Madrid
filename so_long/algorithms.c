1c2d5c28b960c57db2656be74b57afc05fd98d3b
Souls <dmonto01@ucm.es>
2022-11-01 19:38:16 +0100

    Hecho el tsp por si quiero implementar IA

--- so_long/algorithms.c
+++ so_long/algorithms.c
@@ -28,3 +28,23 @@ void bfs(int s, int *dist, int *visited, int **adj, int *queue)
 		i++;
 	}
 }
+
+/*
+SACADO DE MIS APUNTES DE PCOM
+devuelve el coste de ir desde pos al origen (el vértice 0)
+pasando por todos los vértices no visitados (con un bit a 0)
+V vértices del grafo completo
+dist matriz de adyacencia del grafo
+memo tabla de DP
+*/
+int tsp(int pos, int visitados, int V, int **dist, int **memo) {
+	if (visitados == (1 << V) - 1) // hemos visitado ya todos los vértices
+		return dist[pos][0]; // volvemos al origen
+	if (memo[pos][visitados] != -1)
+		return memo[pos][visitados];
+	int res = 1000000000; // INF
+	for (int i = 1; i < V; ++i)
+		if (!(visitados & (1 << i))) // no hemos visitado vértice i
+	res = min(res, dist[pos][i] + tsp(i, visitados | (1 << i), V, dist, memo));
+	return memo[pos][visitados] = res;
+}
