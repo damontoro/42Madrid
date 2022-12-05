89a56a11ed9b63eae2a4f908918619e6896fbc37
David Montoro couso <dmontoro@c3r13s5.42madrid.com>
2022-12-05 13:25:25 +0100

    Pruebas

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -12,33 +12,31 @@ int main(int argc, char *const argv[], char *const envp[])
 	int f = open("pruebas.txt", O_RDONLY);
 
 	dup2(f, STDIN_FILENO);
-	close(f);
 
-	childpid = fork();
-	if(childpid == 0)
+	for(int i = 0; i < 2; i++)
 	{
-		close(fd[0]);
-		dup2(fd[1], STDOUT_FILENO);
-		close(fd[1]);
-		execve("/bin/grep", ma, envp);
+		dup2(f, STDIN_FILENO);
+		childpid = fork();
+		if(childpid == 0)
+		{
+			waitpid(childpid, NULL, 0);
+			printf("Una vuelta1\n");
+			/*close(fd[0]);
+			dup2(fd[1], STDOUT_FILENO);
+			close(fd[1]);
+			execve("/usr/bin/grep", ma, envp);*/
+
+		}
+		else
+		{
+			char c;
+			printf("Una vuelta2\n");
+			close(fd[1]);
+			//dup2(fd[0], STDIN_FILENO);
+			//close(fd[0]);
+			execve("/usr/bin/wc", ma2, envp);
+
+		}
 	}
-	else
-	{
-		char c;
-
-		close(fd[1]);
-
-		/*
-		while (read(fd[0], &c, 1) > 0)
-			write(1, &c, 1);
-		*/
-		printf
-		dup2(fd[0], STDIN_FILENO);
-		close(fd[0]);
-		waitpid(childpid, NULL, 0);
-		execve("/bin/wc", ma2, envp);
-
-	}
-
 	return (0);
 }
