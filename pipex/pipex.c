50b0bbfc733d893147d5b496460b235530c34e6c
Souls <dmonto01@ucm.es>
2023-02-02 10:38:11 +0100

    Acabado, tengo que probar los tests en el campus

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -1,6 +1,5 @@
 #include "pipex.h"
 
-
 void freeMemoria(char **c, char *path, char *aux)
 {
 	int j;
@@ -55,71 +54,58 @@ void exCommand(char *path, char **c, char *const envp[])
 	execve(path, c, envp);
 }
 
-struct data{
-	char **comm;
-	char *path;
-	char *aux;
-	int fd[2];
-	int inFile;
-	int outFile;
-	pid_t childpid;
-};
-
 void iniData(struct data *d, char *const argv[], const int argc){
 	d->inFile = open(argv[1], O_RDONLY, 0666);
 	d->outFile = open(argv[argc - 1],O_RDWR |  O_CREAT | O_TRUNC, 0666);
-	if(pipe(d->fd) == -1)
-	{
-		perror("Error: pipe");
-		exit(1);
-	}
 	dup2(d->inFile, STDIN_FILENO); //Clonamos stdin en el fichero para leer del fichero
 	close(d->inFile);
+}
+
+void manageChild(struct data *d, int argc, int i, int fd[], char *const envp[]){
+	d->childpid = fork();
+	if(d->childpid == 0)
+	{
+		close(fd[0]);
+		if (i == argc - 2)
+		{
+			dup2(d->outFile, STDOUT_FILENO); //Clonamos la salida estandar en el fichero para escribir en el fichero
+			close(d->outFile);
+		}
+		else{
+			dup2(fd[1], STDOUT_FILENO); //Clonamos la salida estandar en el pipe para escribir en el pipe
+			close(fd[1]);
+		}
+		exCommand(d->path,d->comm, envp);
+	}
+	else
+	{
+		close(fd[1]);
+		dup2(fd[0], STDIN_FILENO); //Clonamos stdin en el pipe para leer del pipe
+		close(fd[0]);
+		freeMemoria(d->comm, d->path, d->aux);
+	}
 }
 
 int main(int argc, char *const argv[], char *const envp[])
 {
 	struct data d;
 	int i;
+	int fd[2];
 
 	if (argc < 5)
 	{
 		perror("Error: numero de argumentos incorrecto\n");
 		exit(1);
 	}
-	// el 0 es reading el 1 es writing, para usar uno cerrar el otro
-
 	iniData(&d, argv, argc);
 	i = 2;
 	while (i < argc - 1)
 	{
+		pipe(fd);
 		d.comm = ft_split(argv[i], ' ');
 		d.aux = ft_strjoin("/", d.comm[0]);
 		d.path = findPath(envp, d.aux);
-
-		d.childpid = fork();
-		if(d.childpid == 0)
-		{
-			close(d.fd[0]);
-			if (i == argc - 2)
-			{
-				dup2(d.outFile, STDOUT_FILENO); //Clonamos la salida estandar en el fichero para escribir en el fichero
-				close(d.outFile);
-			}
-			else{
-				dup2(d.fd[1], STDOUT_FILENO); //Clonamos la salida estandar en el pipe para escribir en el pipe
-				close(d.fd[1]);
-			}
-			exCommand(d.path,d.comm, envp);
-		}
-		else
-		{
-			waitpid(d.childpid, NULL, 0);
-			close(d.fd[1]);
-			dup2(d.fd[0], STDIN_FILENO); //Clonamos stdin en el pipe para leer del pipe
-			close(d.fd[0]);
-			freeMemoria(d.comm, d.path, d.aux);
-		}
+		manageChild(&d, argc, i, fd, envp);
 		i++;
 	}
 	return (0);
--- pipex/pipex.h
+++ pipex/pipex.h
@@ -5,3 +5,12 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 #include "libs/libft/libft.h"
+
+struct data{
+	char **comm;
+	char *path;
+	char *aux;
+	int inFile;
+	int outFile;
+	pid_t childpid;
+};
