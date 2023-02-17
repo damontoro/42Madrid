75114ed25d7ab458c059fc53979f7c600dd9462e
David Montoro couso <dmontoro@c1r2s3.42madrid.com>
2023-02-17 12:40:50 +0100

    Creo que está pa corregir

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmontoro <dmontoro@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/17 10:41:18 by dmontoro          #+#    #+#             */
+/*   Updated: 2023/02/17 12:35:47 by dmontoro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "pipex.h"
 
-void freeMemoria(char **c, char *path, char *aux)
+void	free_memoria(char **c, char *path, char *aux)
 {
-	int j;
+	int	j;
 
 	j = 0;
 	while (c[j] != NULL)
@@ -15,98 +27,56 @@ void freeMemoria(char **c, char *path, char *aux)
 	free(aux);
 }
 
-char *findPath(char * const*envp, char *command){
-	int		i;
-	char	**paths;
-	int		j;
-	char	*path;
-	char	*ret = NULL;
-	
-	i = 0;
-	while (envp[i] != NULL && ft_strncmp(envp[i], "PATH=", 5) != 0)
-		i++;
-	paths = ft_split(envp[i], ':');
-	j = -1;
-	while (paths[j] != NULL)
-	{
-		if (j == -1)
-			path = ft_strjoin("", command);
-		else
-			path = ft_strjoin(paths[j], command);
-		j++;
-		if(access(path, F_OK | X_OK) == 0)
-		{
-			ret = ft_strdup(path);
-			break;
-		}
-		free(path);
-	}
-	if(ret == NULL)
+void	ex_command(char *path, char **c, char *const *envp)
+{
+	free(c[0]);
+	c[0] = path;
+	if (!path)
 	{
 		perror("Error: command not found");
 		exit(127);
 	}
-	j = 0;
-	while (paths[j] != NULL)
-	{
-		free(paths[j]);
-		j++;
-	}
-	free(paths);
-	return (ret);
-}
-
-void exCommand(char *path, char **c, char *const envp[])
-{
-	free(c[0]);
-	c[0] = path;
 	if (execve(path, c, envp) == -1)
 	{
 		perror("Error: execve");
 		exit(1);
-	} //El codigo acaba aqui
-
+	}
 }
 
-void iniData(struct data *d, char *const argv[], const int argc){
-	d->inFile = open(argv[1], O_RDONLY | O_CLOEXEC, 0600);
-	d->outFile = open(argv[argc - 1],O_CREAT | O_WRONLY | O_TRUNC | O_CLOEXEC, 0600);
-	dup2(d->inFile, STDIN_FILENO); //Clonamos stdin en el fichero para leer del fichero
-	close(d->inFile);
-}
-
-void manageChild(struct data *d, int argc, int i, int fd[], char *const envp[]){
-	d->childpid = fork();
-	if(d->childpid == 0)
+void	ini_data(t_data *d, char *const argv[], const int argc)
+{
+	d->in_file = open(argv[1], O_RDONLY | O_CLOEXEC, 0600);
+	d->out_file = open(argv[argc - 1], O_CREAT | \
+	O_WRONLY | O_TRUNC | O_CLOEXEC, 0600);
+	if (d->in_file != -1)
 	{
-		close(fd[0]);
-		if (i == argc - 2)
-		{
-			if (d->outFile == -1)
-				exit(1);
-			dup2(d->outFile, STDOUT_FILENO); //Clonamos la salida estandar en el fichero para escribir en el fichero
-			close(d->outFile);
-		}
-		else{
-			dup2(fd[1], STDOUT_FILENO); //Clonamos la salida estandar en el pipe para escribir en el pipe
-			close(fd[1]);
-		}
-		exCommand(d->path,d->comm, envp);
+		dup2(d->in_file, STDIN_FILENO);
+		close(d->in_file);
+	}
+}
+
+void	manage_child(t_data *d, int argc, \
+int i, char *const envp[])
+{
+	d->childpid = fork();
+	if (d->childpid == 0)
+	{
+		manage_child_fds(i, argc, d);
+		ex_command(d->path, d->comm, envp);
 	}
 	else
 	{
-		close(fd[1]);
-		dup2(fd[0], STDIN_FILENO); //Clonamos stdin en el pipe para leer del pipe
-		close(fd[0]);
-		freeMemoria(d->comm, d->path, d->aux);
+		close(d->fd[1]);
+		dup2(d->fd[0], STDIN_FILENO);
+		close(d->fd[0]);
+		free_memoria(d->comm, d->path, d->aux);
 	}
 }
 
-int main(int argc, char *const argv[], char *const envp[])
+int	main(int argc, char *const argv[], char *const envp[])
 {
-	struct data d;
-	int i;
-	int fd[2];
+	t_data	d;
+	int		i;
 
 	if (!envp || !*envp)
 		exit(127);
@@ -115,24 +85,17 @@ int main(int argc, char *const argv[], char *const envp[])
 		perror("Error: numero de argumentos incorrecto\n");
 		exit(1);
 	}
-	iniData(&d, argv, argc);
+	ini_data(&d, argv, argc);
 	i = 2;
 	while (i < argc - 1)
 	{
-		pipe(fd);
+		pipe(d.fd);
 		d.comm = ft_split(argv[i], ' ');
 		d.aux = ft_strjoin("/", d.comm[0]);
-		d.path = findPath(envp, d.aux);
-		manageChild(&d, argc, i, fd, envp);
-		if(i == argc - 2)
-		{
-			waitpid(d.childpid, &d.exit, 0);
-			if (WIFEXITED(d.exit))
-				d.exit = WEXITSTATUS(d.exit);
-			printf("exit status: %d\n", d.exit);
-		}
+		d.path = find_path(envp, d.aux);
+		manage_child(&d, argc, i, envp);
+		last_child(i, argc, &d);
 		i++;
 	}
 	return (d.exit);
 }
-
--- pipex/pipex.h
+++ pipex/pipex.h
@@ -1,22 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmontoro <dmontoro@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/17 11:04:51 by dmontoro          #+#    #+#             */
+/*   Updated: 2023/02/17 11:38:41 by dmontoro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PIPEX_H
 # define PIPEX_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <fcntl.h>
-#include "libs/libft/libft.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <fcntl.h>
+# include "libs/libft/libft.h"
 
-struct data{
-	char **comm;
-	char *path;
-	char *aux;
-	int inFile;
-	int outFile;
-	int exit;
-	pid_t childpid;
-};
+typedef struct s_data{
+	char	**comm;
+	char	*path;
+	char	*aux;
+	int		in_file;
+	int		out_file;
+	int		exit;
+	pid_t	childpid;
+	int		fd[2];
+}	t_data;
 
-# endif
+char	*find_path(char *const *envp, char *command);
+void	last_child(int i, int argc, t_data *d);
+void	manage_child_fds(int i, int argc, t_data *d);
+
+#endif
