8aa0d0a932f8c84c009ba06401ce6c3ed0620a68
David Montoro couso <dmontoro@c1r12s4.42madrid.com>
2023-07-22 09:52:24 +0200

    Comprobados los leaks y creada una regla para probar

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -6,21 +6,25 @@
 /*   By: dmontoro <dmontoro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 08:28:20 by dmontoro          #+#    #+#             */
-/*   Updated: 2023/07/20 13:13:36 by dmontoro         ###   ########.fr       */
+/*   Updated: 2023/07/22 09:19:33 by dmontoro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+// void ex(){
+// 	system("leaks push_swap");
+// }
 
 int	main(int argc, char **argv)
 {
 	t_stack	*stack_a;
 	t_stack	*stack_b;
 
-	stack_a = (t_stack *)calloc(1, (sizeof(t_stack)));
-	stack_b = (t_stack *)calloc(1, (sizeof(t_stack)));
+	stack_a = (t_stack *)ft_calloc(1, (sizeof(t_stack)));
+	stack_b = (t_stack *)ft_calloc(1, (sizeof(t_stack)));
 	if (argc <= 1)
-		exit(0);
+		ft_error("Error\n");
 	parse_args(stack_a, argc, argv);
 	index_stack(stack_a);
 	if (!sorted(stack_a->top))
