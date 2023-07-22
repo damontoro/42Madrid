103da977a3206a724f9c322237f62104216766cc
David Montoro couso <dmontoro@c1r12s4.42madrid.com>
2023-07-22 11:36:06 +0200

    Me salvo una corrección, una cosa no es como pedian, cambiado

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: dmontoro <dmontoro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 08:28:20 by dmontoro          #+#    #+#             */
-/*   Updated: 2023/07/22 09:19:33 by dmontoro         ###   ########.fr       */
+/*   Updated: 2023/07/22 11:21:39 by dmontoro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int	main(int argc, char **argv)
 	stack_a = (t_stack *)ft_calloc(1, (sizeof(t_stack)));
 	stack_b = (t_stack *)ft_calloc(1, (sizeof(t_stack)));
 	if (argc <= 1)
-		ft_error("Error\n");
+		return (0);
 	parse_args(stack_a, argc, argv);
 	index_stack(stack_a);
 	if (!sorted(stack_a->top))
