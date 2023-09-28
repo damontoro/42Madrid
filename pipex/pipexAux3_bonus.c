ef0824b0a18758cab53b8f4e6b5581bf75f6283d
David Montoro couso <dmontoro@c3r10s6.42madrid.com>
2023-09-28 19:11:49 +0200

    Fixed fd leaks and prepared for the correction

---
+++ pipex/pipexAux3_bonus.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipexAux3_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmontoro <dmontoro@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/12 11:50:07 by dmontoro          #+#    #+#             */
+/*   Updated: 2023/09/28 19:04:03 by dmontoro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex_bonus.h"
+
+static void	jump_quotes(char const *s, int *i, int *count)
+{
+	if (s[*i] == '\'')
+	{
+		while (s[++(*i)] && s[*i] != '\'')
+			;
+		(*i)++;
+		(*count)++;
+	}
+	if (s[*i] == '\"')
+	{
+		while (s[++(*i)] && s[*i] != '\"')
+			;
+		(*i)++;
+		(*count)++;
+	}
+}
+
+int	count_words(char const *s, char c)
+{
+	int	count;
+	int	found;
+	int	i;
+
+	i = 0;
+	found = 0;
+	count = 0;
+	while (s[i])
+	{
+		jump_quotes(s, &i, &count);
+		while (s[i] != c && s[i])
+		{
+			if (!found)
+			{
+				count++;
+				found = 1;
+			}
+			++i;
+		}
+		while (s[i] == c && s[i++])
+			found = 0;
+	}
+	return (count);
+}
+
+int	check_comillas(char c, const char *s, int i)
+{
+	int	j;
+
+	if (s[i] != c)
+		return (0);
+	j = i + 1;
+	while (s[j] && s[j] != c)
+		j++;
+	if (s[j] == c)
+		return (j - i);
+	return (0);
+}
+
+void	ft_error(char *str)
+{
+	perror(str);
+	exit(1);
+}
